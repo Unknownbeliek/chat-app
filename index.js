ffcbc4132de1bd7e3ae6a316c9c8bde745cccf0b
Unknownbeliek <rajkumarpersonal7@gmail.com>
2026-07-16 17:46:17 +0530

    add connection btw websocket working chatapp

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+import { log } from 'node:console'; 
 import http from 'node:http'
+import fs from 'node:fs/promises'
+import path from 'path';
+import { WebSocketServer } from 'ws';
+const PORT = process.env.PORT ?? 9000;
+
+const httpServer = http.createServer(async function (req,res) {
+    const indexFile = await fs.readFile(path.resolve('./index.html'), 'utf-8');
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(indexFile);
+});
+const wsServer =new WebSocketServer({server : httpServer});
+
+wsServer.on('connection' , (websocket)=>{
+    console.log(`WebSocket Connection....`);
+    websocket.on('message',(data)=>{
+        console.log(`websokcet message Recv...` ,data.toString());
+        wsServer.clients.forEach((client)=>{
+            client.send(data.toString());
+        });
+        
+    });
+});
+
+httpServer.listen(PORT, () =>{
+    console.log(`server is running on on http://localhost:${PORT}`);
+    
+})
